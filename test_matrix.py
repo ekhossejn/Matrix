f33cf69f759cdfccd4cbe9f55c42f536799a2a04
ekhossejn <45770055+ekhossejn@users.noreply.github.com>
2019-09-22 14:45:48 +0300

    Update test_matrix.py

--- test_matrix.py
+++ test_matrix.py
@@ -1,8 +1,15 @@
 from matrix import MyMatrix
 
+def test_init():
+    matrix = [[1], [2], [3]]
+    mx = MyMatrix(matrix)
+    assert(mx.get_data() == [[1], [2], [3]])
+    matrix.pop(-1)
+    assert(mx.get_data() == [[1], [2], [3]])
+    
 def test_repr():
     array = [[-90, 10], [2, -2], [90, 10]]
-    assert(MyMatrix(array).__repr__() =='-90  10\n  2  -2\n 90  10')
+    assert(MyMatrix(array).__repr__() == '-90  10\n  2  -2\n 90  10')
     assert(MyMatrix([]).__repr__() == '')
     
 def test_size():
@@ -17,6 +24,9 @@ def test_flip_up_down():
     assert(mx.get_data() == [[4, 5, 6], [1, 2, 3]])
     mx.flip_up_down()
     assert(mx.get_data() == [[1, 2, 3], [4, 5, 6]])
+    mx = MyMatrix([])
+    mx.flip_up_down()
+    assert(mx.get_data() == [])
 
 def test_flip_left_right():
     mx = MyMatrix([[1, 2, 3], [4, 5, 6]])
@@ -24,6 +34,9 @@ def test_flip_left_right():
     assert(mx.get_data() == [[3, 2, 1], [6, 5, 4]])
     mx.flip_left_right()
     assert(mx.get_data() == [[1, 2, 3], [4, 5, 6]])
+    mx = MyMatrix([])
+    mx.flip_up_down()
+    assert(mx.get_data() == [])
 
 def test_flipped_up_down():
     mx = MyMatrix([[1, 2, 3], [4, 5, 6]])
